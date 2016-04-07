d0de6ba762ca5c8a1e8840991a389145096d445c
eriksoderberg15 <dat12eso@student.lu.se>
2016-04-07 13:35:44 +0200

    pushing Main and updated Database

--- Krusty_Cookies/src/Database.java
+++ Krusty_Cookies/src/Database.java
@@ -7,5 +7,57 @@ import java.util.*;
  */
 
 public class Database {
+	/**
+	 * The database connection.
+	 */
+	private Connection conn;
 
+	/**
+	 * Create the database interface object. Connection to the database is
+	 * performed later.
+	 */
+	public Database() {
+		conn = null;
+	}
+	
+	/**
+	 * Open a connection to the database, using the specified user name and
+	 * password.
+	 * 
+	 * @param userName
+	 *            The user name.
+	 * @param password
+	 *            The user's password.
+	 * @return true if the connection succeeded, false if the supplied user name
+	 *         and password were not recognized. Returns false also if the JDBC
+	 *         driver isn't found.
+	 */
+	public boolean openConnection(String userName, String password) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://puccini.cs.lth.se/" + userName, userName,
+					password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Close the connection to the database.
+	 */
+	public void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+		}
+		conn = null;
+	}
 }
---
+++ Krusty_Cookies/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	/*
+	 * Main class creating the database instance and setting up the connection
+	 * 
+	 * Also creating and booting the GUI
+	 */
+	public static void main(String[] args) {
+
+		/*
+		 * 1. Create database object
+		 * 2. Set up connection
+		 * 3. Start GUI
+		 */
+	}
+
+}
